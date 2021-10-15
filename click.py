2e613c2c29bac17c6754624519f85ebc26b27d37
robertoAureli <r.aureli@protonmail.com>
2021-10-15 23:05:20 +0200

    click and break

---
+++ click.py
@@ -0,0 +1,12 @@
+import keyboard
+import pyautogui,time
+
+import keyboard 
+while True:  
+        if keyboard.is_pressed('x'):   
+            for i in range(300):
+                pyautogui.click()
+            
+        if keyboard.is_pressed('q'):  
+                break
+   
